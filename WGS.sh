faa0eb2d844d285fb8d3b275577f773f7cd0c5dc
Colin Reid <cpreid2@gmail.com>
2017-04-02 12:21:13 -0500

    Update WGS.sh

--- WGS.sh
+++ WGS.sh
@@ -1,3 +1,4 @@
+# Script to parse read group information from a set of SRA Accession ID's
 while IFS='' read -r line || [[ -n "$line" ]]; do
     sam-dump -r $line | head -n 1000 | grep '@RG' > $line'_ReadGroups.txt'
     rm $line.sra.cache 
